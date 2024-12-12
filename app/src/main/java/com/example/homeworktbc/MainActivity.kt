650df3b95151d142c2f258fa3b135021df744e02
Gvichoo <saba.gvichiani27@gmail.com>
2024-12-12 17:31:25 +0400

    Figma

--- app/src/main/java/com/example/homeworktbc/MainActivity.kt
+++ app/src/main/java/com/example/homeworktbc/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.example.homeworktbc
 
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.homeworktbc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        text1()
+    }
+    fun text1(){
+        binding.textView1.text = "Overview"
+        binding.textView3.text = "8 hours"
+        binding.celsiusET.text = "16  C"
+        binding.textview4.text = "4.5"
+        binding.textView6.text = "Details"
+        binding.button.text = "Book Now"
+        binding.textviewINFO.text ="This vast mountain range is renowned for its remarkable diversity in terms of topography and climate. It features towering peaks, active volcanoes, deep canyons, expansive plateaus, and lush valleys. The Andes are also home to "
+        binding.Andes.text="Andes Mountain"
+        binding.south.text = "South, America"
+        binding.price.text = "Price"
+        binding.dollar.text = "$230"
     }
 }
