6521ac69a302e7dfb194d6085f8ed7028f9df519
Gvichoo <saba.gvichiani27@gmail.com>
2024-12-17 21:39:19 +0400

    Users

---
+++ app/src/main/java/User.kt
@@ -0,0 +1,38 @@
+import android.os.Parcel
+import android.os.Parcelable
+
+data class User (
+    val firstName: String,
+    val lastName: String,
+    val age: Int,
+    val email: String
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        parcel.readString()?: "",
+        parcel.readInt(),
+        parcel.readString()?: ""
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(firstName)
+        parcel.writeString(lastName)
+        parcel.writeInt(age)
+        parcel.writeString(email)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<User> {
+        override fun createFromParcel(parcel: Parcel): User {
+            return User(parcel)
+        }
+
+        override fun newArray(size: Int): Array<User?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
--- app/src/main/java/com/example/homeworktbc/MainActivity.kt
+++ app/src/main/java/com/example/homeworktbc/MainActivity.kt
@@ -1,20 +1,101 @@
 package com.example.homeworktbc
 
+import User
+import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Patterns
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.homeworktbc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val users = mutableListOf<User>()
+    private var activeUsers = 0
+    private var removedUsers = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.addUserBtn.setOnClickListener {
+            val firstname = binding.etFirstName.text.toString()
+            val lastname = binding.etLastName.text.toString()
+            val age = binding.etAge.text.toString()
+            val email = binding.etEmail.text.toString()
+
+
+            if (firstname.isEmpty() || lastname.isEmpty() || age.isEmpty() || email.isEmpty()) {
+                showOperation("All fields must be filled!", Color.RED)
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                showOperation("Invalid email!", Color.RED)
+                return@setOnClickListener
+            }
+            val user = User(
+                firstName = binding.etFirstName.text.toString(),
+                lastName = binding.etLastName.text.toString(),
+                age = binding.etAge.text.toString().toInt(),
+                email = binding.etEmail.text.toString()
+            )
+
+
+            if (users.any { it.email == user.email }) {
+                showOperation("User already exists!", Color.RED)
+            } else {
+                users.add(user)
+                activeUsers++
+                updateCounts()
+                showOperation("User added successfully!", Color.GREEN)
+            }
+        }
+
+        binding.removeUserBtn.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+
+            val user = users.find { it.email == email }
+            if (user != null) {
+                users.remove(user)
+                activeUsers--
+                removedUsers++
+                updateCounts()
+                showOperation("User deleted successfully!", Color.GREEN)
+            } else {
+                showOperation("User does not exist!", Color.RED)
+            }
+        }
+
+        binding.updateUserBtn.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+
+            val user = users.find { it.email == email }
+            if (user != null) {
+                val updatedUser = User(
+                    firstName = binding.etFirstName.text.toString(),
+                    lastName = binding.etLastName.text.toString(),
+                    age = binding.etAge.text.toString().toIntOrNull() ?: 0,
+                    email = email
+                )
+                users[users.indexOf(user)] = updatedUser
+                showOperation("User updated successfully!", Color.GREEN)
+            } else {
+                showOperation("User does not exist!", Color.RED)
+            }
         }
     }
+
+    private fun updateCounts() {
+        binding.activeUsersTV.text = "Active Users: $activeUsers"
+        binding.removedUsersTV.text = "Deleted Users: $removedUsers"
+    }
+
+    private fun showOperation(outPut: String, color: Int) {
+        binding.operation.text = outPut
+        binding.operation.setTextColor(color)
+    }
 }
+
